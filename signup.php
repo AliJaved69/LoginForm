0c107353479ea4e81ed11b9cca010d544bcb5961
AliJaved69 <m.alijaved69@gmail.com>
2023-09-06 18:21:30 +0500

    new changes

--- signup.php
+++ signup.php
@@ -1,7 +1,17 @@
 <?php
 
 $host = "localhost";
-$username = "admin";
+$username = "root";
 $password = "";
 $db_name = "newdb";
+
+$conn = mysqli_connect($host,$username,$password,$db_name);
+
+if($conn->connect_error){
+    die("Connection error: " .$conn->connect_error);
+}else{
+    echo "Good!";
+}
+
+
 ?>
