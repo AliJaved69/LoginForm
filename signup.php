3063ec31e052c8a310b0ed9c79ed38df08f384b7
AliJaved69 <m.alijaved69@gmail.com>
2023-09-06 19:53:13 +0500

    new changes

--- signup.php
+++ signup.php
@@ -8,21 +8,7 @@ $db_name = "newdb";
 $conn = mysqli_connect($host,$username,$password,$db_name);
 
 if($conn->connect_error){
-    die("Connection error: " .$conn->connect_error);
-}
-
-if($_SERVER["REQUEST_METHOD"] == "POST")
-{
-  $fname = htmlspecialchars($_POST['fname']);
-  $lname = htmlspecialchars($_POST['lname']);
-  $email = htmlspecialchars($_POST['email']);
-  $password = htmlspecialchars($_POST['password']);
-
-  if (empty($fname) || empty($lname) || empty($email) || empty($password)) {
-    echo "Please fill in all the fields.";
-  } else {
-    header("Location: signupform.html", 302);
-  }
+  die("Connection Failed: " . $conn->connect_error)
 }
 
 
