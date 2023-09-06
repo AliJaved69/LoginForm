b16afc6c5c9e3c0c7718c73e47b45b3a72464d3f
AliJaved69 <m.alijaved69@gmail.com>
2023-09-06 18:38:51 +0500

    new upadtion

--- signup.php
+++ signup.php
@@ -9,8 +9,20 @@ $conn = mysqli_connect($host,$username,$password,$db_name);
 
 if($conn->connect_error){
     die("Connection error: " .$conn->connect_error);
-}else{
-    echo "Good!";
+}
+
+if($_SERVER["REQUEST_METHOD"] == "POST")
+{
+  $fname = htmlspecialchars($_POST['fname']);
+  $lname = htmlspecialchars($_POST['lname']);
+  $email = htmlspecialchars($_POST['email']);
+  $password = htmlspecialchars($_POST['password']);
+
+  if (empty($fname) || empty($lname) || empty($email) || empty($password)) {
+    echo "Please fill in all the fields.";
+  } else {
+    header("Location: signupform.html", 302);
+  }
 }
 
 
